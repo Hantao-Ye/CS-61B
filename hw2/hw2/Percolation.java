9cc613e78e1866699eae66819f245388b5d9990c
Hantao-Ye <hantaoye2000@gmail.com>
2022-06-12 05:26:00 +0800

    refactor hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -4,10 +4,9 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     private int n;
-    private int size;
-    private WeightedQuickUnionUF arr;
-    private boolean[] openArr;
-    private boolean[] fullArr;
+    private boolean[][] openArr;
+    private WeightedQuickUnionUF fullArr;
+    private WeightedQuickUnionUF percArr;
 
     /**
      * Calculate the index of array in [r, c]
@@ -27,10 +26,12 @@ public class Percolation {
         }
 
         n = N;
-        size = N * N;
-        arr = new WeightedQuickUnionUF(size);
-        openArr = new boolean[size];
-        fullArr = new boolean[size];
+        // assume fullArr[N * N] as the virtual head
+        fullArr = new WeightedQuickUnionUF(N * N + 1);
+        // assume percArr[N * N] as the virtual head, [N * N + 1] as tail
+        percArr = new WeightedQuickUnionUF(N * N + 2);
+
+        openArr = new boolean[N][N];
     }
 
     // open the site (row, col) if it is not open already
@@ -39,52 +40,11 @@ public class Percolation {
             throw new IndexOutOfBoundsException();
         }
 
-        int ptr = xyTo1D(row, col);
-        openArr[ptr] = true;
-
-        int left = ptr - 1;
-        int right = ptr + 1;
-        int up = ptr - n;
-        int down = ptr + n;
-
-        // avoid side
-        if (row == 0) {
-            up = -1;
-        }
-        if (row == n - 1) {
-            down = -1;
-        }
-        if (col == 0) {
-            left = -1;
-        }
-        if (col == n - 1) {
-            right = -1;
-        }
-
-        // union the open cell
-        if (up != -1 && openArr[up]) {
-            arr.union(ptr, up);
-        }
-        if (down != -1 && openArr[down]) {
-            arr.union(ptr, down);
-        }
-        if (left != -1 && openArr[left]) {
-            arr.union(ptr, left);
-        }
-        if (right != -1 && openArr[right]) {
-            arr.union(ptr, right);
-        }
-
-        // when hit the top, full the cells
-        if (row == 0 || (up != -1 && fullArr[up]) || (down != -1 && fullArr[down])
-                || (left != -1 && fullArr[left]) || (right != -1 && fullArr[right])) {
-            fullArr[ptr] = true;
-            fullOpen(row, col);
-        }
-
+        openArr[row][col] = true;
+        union(row, col);
     }
 
-    private void fullOpen(int row, int col) {
+    private void union(int row, int col) {
         int ptr = xyTo1D(row, col);
 
         int up = ptr - n;
@@ -92,11 +52,13 @@ public class Percolation {
         int left = ptr - 1;
         int right = ptr + 1;
 
-        // avoid side
         if (row == 0) {
+            fullArr.union(ptr, n * n);
+            percArr.union(ptr, n * n);
             up = -1;
         }
         if (row == n - 1) {
+            percArr.union(ptr, n * n + 1);
             down = -1;
         }
         if (col == 0) {
@@ -106,54 +68,21 @@ public class Percolation {
             right = -1;
         }
 
-        // stop when hit full block
-        if (up != -1 && fullArr[up]) {
-            up = -1;
+        if (up != -1 && openArr[row - 1][col]) {
+            fullArr.union(up, ptr);
+            percArr.union(up, ptr);
         }
-        if (down != -1 && fullArr[down]) {
-            down = -1;
+        if (down != -1 && openArr[row + 1][col]) {
+            fullArr.union(down, ptr);
+            percArr.union(down, ptr);
         }
-        if (left != -1 && fullArr[left]) {
-            left = -1;
+        if (left != -1 && openArr[row][col - 1]) {
+            fullArr.union(left, ptr);
+            percArr.union(left, ptr);
         }
-        if (right != -1 && fullArr[right]) {
-            right = -1;
-        }
-
-        // make the side cell full
-        if (up != -1 && arr.connected(ptr, up)) {
-            fullArr[up] = true;
-        } else {
-            up = -1;
-        }
-        if (down != -1 && arr.connected(ptr, down)) {
-            fullArr[down] = true;
-        } else {
-            down = -1;
-        }
-        if (left != -1 && arr.connected(ptr, left)) {
-            fullArr[left] = true;
-        } else {
-            left = -1;
-        }
-        if (right != -1 && arr.connected(ptr, right)) {
-            fullArr[right] = true;
-        } else {
-            right = -1;
-        }
-
-        // recursive loop
-        if (up != -1) {
-            fullOpen(row - 1, col);
-        }
-        if (down != -1) {
-            fullOpen(row + 1, col);
-        }
-        if (left != -1) {
-            fullOpen(row, col - 1);
-        }
-        if (right != -1) {
-            fullOpen(row, col + 1);
+        if (right != -1 && openArr[row][col + 1]) {
+            fullArr.union(right, ptr);
+            percArr.union(right, ptr);
         }
     }
 
@@ -163,7 +92,7 @@ public class Percolation {
             throw new IndexOutOfBoundsException();
         }
 
-        return openArr[xyTo1D(row, col)];
+        return openArr[row][col];
     }
 
     // is the site (row, col) full?
@@ -172,15 +101,17 @@ public class Percolation {
             throw new IndexOutOfBoundsException();
         }
 
-        return fullArr[xyTo1D(row, col)];
+        return fullArr.connected(n * n, xyTo1D(row, col));
     }
 
     // number of open sites
     public int numberOfOpenSites() {
         int count = 0;
-        for (boolean open : openArr) {
-            if (open) {
-                count++;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (openArr[i][j]) {
+                    count++;
+                }
             }
         }
         return count;
@@ -188,12 +119,7 @@ public class Percolation {
 
     // does the system percolate?
     public boolean percolates() {
-        for (int i = size - n; i < size; i++) {
-            if (fullArr[i]) {
-                return true;
-            }
-        }
-        return false;
+        return percArr.connected(n * n, n * n + 1);
     }
 
     // use for unit testing (not required)
